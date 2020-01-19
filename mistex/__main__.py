0863a765d35814c58bf11685bd829f8ca043f952
Martino Sorbaro <martino.sorbaro@ed.ac.uk>
2020-01-19 21:28:24 +0100

    created executable package

---
+++ mistex/__main__.py
@@ -0,0 +1,22 @@
+import mistex
+import os
+import sys
+
+if len(sys.argv) <= 1:
+    raise ValueError('No input files.')
+
+file = sys.argv[1]
+r = mistex.latex.read(file)
+fname = file.strip('.md')
+
+with open(f'{fname}.tex', 'w') as F:
+    F.write(r)
+
+# sh = (
+#       f'xelatex -shell-escape {fname}.tex '
+#       f'&& open {fname}.pdf; '
+#       f'rm {fname}' + '.{log,aux,out};'
+#       f'rm -r _minted-{fname}'
+# )
+#
+# os.system(sh)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mistex',
+    version='0.0.1',
+    author='Martino Sorbaro',
+    author_email='martino.sorbaro@posteo.net',
+    packages=['mistex'],
+    install_requires=['mistune']
+)
