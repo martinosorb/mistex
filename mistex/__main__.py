7d58667acb7af33a98730e6d338a084be9ddc162
Martino Sorbaro <martino.sorbaro@ed.ac.uk>
2020-01-25 16:13:26 +0100

    using latexmk

--- mistex/__main__.py
+++ mistex/__main__.py
@@ -12,11 +12,11 @@ fname = file.strip('.md')
 with open(f'{fname}.tex', 'w') as F:
     F.write(r)
 
-# sh = (
-#       f'xelatex -shell-escape {fname}.tex '
-#       f'&& open {fname}.pdf; '
-#       f'rm {fname}' + '.{log,aux,out};'
-#       f'rm -r _minted-{fname}'
-# )
-#
-# os.system(sh)
+sh = (
+      f'latexmk -xelatex -shell-escape {fname}.tex '
+      # f'&& open {fname}.pdf; '
+      # f'rm {fname}' + '.{log,aux,out};'
+      # f'rm -r _minted-{fname}'
+)
+
+os.system(sh)
