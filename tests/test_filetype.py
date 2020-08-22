9fb7dca532b93ab0b4eb710f7d6d7ac814ffcea6
Martino Sorbaro <martino.sorbaro@ed.ac.uk>
2020-08-22 16:38:58 +0200

    tests run from any dir; corrected one test

--- tests/test_filetype.py
+++ tests/test_filetype.py
@@ -2,16 +2,19 @@ import pytest
 import os
 
 # find test files
-IN_FILES = os.listdir("in")
-OUT_FILES = os.listdir("out")
+HERE = os.path.dirname(os.path.abspath(__file__))
+IN_DIR = os.path.join(HERE, "in")
+OUT_DIR = os.path.join(HERE, "out")
+IN_FILES = os.listdir(IN_DIR)
+OUT_FILES = os.listdir(OUT_DIR)
 assert len(IN_FILES) == len(OUT_FILES)
 
 
 @pytest.fixture(params=IN_FILES)
 def load_in_out(request):
-    with open(os.path.join("in", request.param)) as file:
+    with open(os.path.join(IN_DIR, request.param)) as file:
         in_file = file.read()
-    with open(os.path.join("out", request.param)) as file:
+    with open(os.path.join(OUT_DIR, request.param)) as file:
         out_file = file.read()
     return in_file, out_file
 
