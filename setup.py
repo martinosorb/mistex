8a1611fefd34f5957a03c3d48847f0740c350b57
Martino Sorbaro <martino.sorbaro@ed.ac.uk>
2020-10-06 22:30:21 +0200

    Improved setup.py, ignored builds for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
-    name='mistex',
-    version='0.0.1',
+    name="mistex",  # Replace with your own username
+    version="0.0.1",
     author='Martino Sorbaro',
     author_email='martino.sorbaro@posteo.net',
-    packages=['mistex'],
-    install_requires=['mistune>=2.0.*']
+    description="A markdown and latex blender",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/martinosorb/mistex",
+    packages=["mistex"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['mistune>=2.0.*'],
 )
