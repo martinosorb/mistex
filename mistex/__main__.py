bda333933dc197e9c2d9cb11797711dc94e581d3
Martino Sorbaro <martino.sorbaro@posteo.net>
2022-06-06 18:43:39 +0200

    seems to work with --out etc.

--- mistex/__main__.py
+++ mistex/__main__.py
@@ -20,39 +20,42 @@ args = parser.parse_args()
 
 if args.input_file is None:
     raise ValueError('No input files.')
+input_file = Path(args.input_file)
 
 if args.out is None:
     # another suffix will be added later, either .pdf or .tex
-    out = Path(args.input_file).with_suffix(".mistex")
+    out = Path(args.input_file).with_suffix(".mistex").name
+    out = Path(out)
 else:
     out = Path(args.out).with_suffix("")
 output_folder = out.parent
 output_folder.mkdir(parents=True, exist_ok=True)
 
 if args.cachedir is None:
-    # we put the cache in output_folder/latex_cache/input_file_name
-    cachedir = output_folder / "mistex_cache" / Path(args.input_file).stem
+    cachedir = Path("mistex_cache") / input_file.stem
 else:
     cachedir = Path(args.cachedir)
+
 
 COMPILER = "latexmk"
 FLAGS = [
     "-pdf",
-    f"--outdir={cachedir}",
+    f"--outdir={cachedir.resolve()}",
     "-xelatex",
     "-shell-escape",
 ]
 
 # run the md parser
-reader = md2latex(cachedir=cachedir)
-pylatex_document = reader.parse(read_file(args.input_file))
+reader = md2latex(cachedir=cachedir.resolve())
+pylatex_document = reader.parse(read_file(input_file))
 
 # save the result to the tmp directory
 if args.pdf:
-    # pdf output name is the name only! the folder is the cache dir
-    pylatex_document.generate_pdf(out.name, compiler=COMPILER, compiler_args=FLAGS)
-    # copy the output PDF from the cache dir to here
-    pdf_out_name = out.name + ".pdf"
-    os.replace(cachedir / pdf_out_name, Path.cwd() / pdf_out_name)
+    # we have to generate where the input is, in order to use the images there
+    # but the pdf will be generated in cachedir and this tex file will be removed.
+    temp_location = input_file.parent / "mistex_out"
+    pylatex_document.generate_pdf(temp_location, compiler=COMPILER, compiler_args=FLAGS, clean_tex=True)
+    # copy the output PDF from the cache dir to the desired output
+    os.replace(cachedir / "mistex_out.pdf", out.with_suffix(".pdf"))
 else:
     pylatex_document.generate_tex(str(out))
